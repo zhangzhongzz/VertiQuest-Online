c5cfdf068536815aee684edba817998cb371c9c1
zhangzhongzz <1352188353@qq.com>
2020-03-11 13:05:47 +0800

    ddd

--- pages/video/video.js
+++ pages/video/video.js
@@ -2,17 +2,26 @@
 Page({
 
   /**
-   * 页面的初始数据
+   * 页面的初始数据: 地址，标题，外加图片，地址需要解码转码
    */
   data: {
-    src: 'http://wxsnsdy.tc.qq.com/105/20210/snsdyvideodownload?filekey=30280201010421301f0201690402534804102ca905ce620b1241b726bc41dcff44e00204012882540400&bizid=1023&hy=SH&fileparam=302c020101042530230204136ffd93020457e3c4ff02024ef202031e8d7f02030f42400204045a320a0201000400'
-  },
-
-  goPlayer: function () {
-    var that = this
-    wx.navigateTo({
-      url: "../player/player?srcData="+that.data.src,
-    })
+    videoUrl:[
+      { src: encodeURIComponent('cloud://vertiquest-e99wl.7665-vertiquest-e99wl-1301271100/tandem站立_2.mp4'), title: 'Tandem站立', imageUrl: 'http://47.100.27.103:8080/Images/tandem站立.png' },
+      { src: encodeURIComponent('cloud://vertiquest-e99wl.7665-vertiquest-e99wl-1301271100/步行练习.mp4'), title: '步行练习', imageUrl: 'http://47.100.27.103:8080/Images/步行.png' },
+      { src: encodeURIComponent('cloud://vertiquest-e99wl.7665-vertiquest-e99wl-1301271100/传球练习_2.mp4'), title: '传球练习', imageUrl: 'http://47.100.27.103:8080/Images/传球.png' },
+      { src: encodeURIComponent('cloud://vertiquest-e99wl.7665-vertiquest-e99wl-1301271100/海绵垫平衡练习.mp4'), title: '海绵平衡练习', imageUrl: 'http://47.100.27.103:8080/Images/海绵平衡练习.png' },
+      { src: encodeURIComponent('cloud://vertiquest-e99wl.7665-vertiquest-e99wl-1301271100/行走中摇头抬头低头.mp4'), title: '行走中摇头抬头低头', imageUrl: 'http://47.100.27.103:8080/Images/行走中摇头.png' },
+      { src: encodeURIComponent('cloud://vertiquest-e99wl.7665-vertiquest-e99wl-1301271100/环形走.mp4'), title: '环形走', imageUrl: 'http://47.100.27.103:8080/Images/环形走.png' },
+      { src: encodeURIComponent('cloud://vertiquest-e99wl.7665-vertiquest-e99wl-1301271100/脚跟脚尖走直线.mp4'), title: '脚跟脚尖走直线', imageUrl: 'http://47.100.27.103:8080/Images/脚跟脚尖直线走.png' },
+      { src: encodeURIComponent('cloud://vertiquest-e99wl.7665-vertiquest-e99wl-1301271100/凝视稳定练习1.mp4'), title: '凝视稳定练习1', imageUrl: 'http://47.100.27.103:8080/Images/凝视稳定1.png' },
+      { src: encodeURIComponent('cloud://vertiquest-e99wl.7665-vertiquest-e99wl-1301271100/凝视稳定练习2.mp4'), title: '凝视稳定练习2', imageUrl: 'http://47.100.27.103:8080/Images/凝视稳定2.png' },
+      { src: encodeURIComponent('cloud://vertiquest-e99wl.7665-vertiquest-e99wl-1301271100/凝视稳定练习3.mp4'), title: '凝视稳定练习3', imageUrl: 'http://47.100.27.103:8080/Images/凝视稳定3.png' },
+      { src: encodeURIComponent('cloud://vertiquest-e99wl.7665-vertiquest-e99wl-1301271100/扫视练习.mp4'), title: '扫视练习', imageUrl: 'http://47.100.27.103:8080/Images/扫视.png' },
+      { src: encodeURIComponent('cloud://vertiquest-e99wl.7665-vertiquest-e99wl-1301271100/视追踪训练.mp4'), title: '视追踪训练', imageUrl: 'http://47.100.27.103:8080/Images/视追踪.png' },
+      { src: encodeURIComponent('cloud://vertiquest-e99wl.7665-vertiquest-e99wl-1301271100/耸肩.mp4'), title: '耸肩', imageUrl: 'http://47.100.27.103:8080/Images/耸肩.png' },
+      { src: encodeURIComponent('cloud://vertiquest-e99wl.7665-vertiquest-e99wl-1301271100/弯腰拾物.mp4'), title: '弯腰拾物', imageUrl: 'http://47.100.27.103:8080/Images/弯腰拾物.png' },
+      { src: encodeURIComponent('cloud://vertiquest-e99wl.7665-vertiquest-e99wl-1301271100/由坐起立.mp4'), title: '由坐起立', imageUrl: 'http://47.100.27.103:8080/Images/由坐起立.png' },
+    ],
   },
 
 
--- utils/util.js
+++ utils/util.js
@@ -1,22 +1,9 @@
-const formatTime = date => {
-  const year = date.getFullYear()
-  const month = date.getMonth() + 1
-  const day = date.getDate()
-  const hour = date.getHours()
-  const minute = date.getMinutes()
-  const second = date.getSeconds()
-
-  return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
-}
 
 const formatNumber = n => {
   n = n.toString()
   return n[1] ? n : '0' + n
 }
 
-module.exports = {
-  formatTime: formatTime
-}
 
 /* 公共request 方法 */
 const requestUrl = ({
@@ -80,3 +67,217 @@ module.exports = {
   requestUrl: requestUrl
 }
 
+
+//新增
+//时间格式化  yyyy/MM/dd HH:mm:ss
+const formatTime = date => {
+  const year = date.getFullYear()
+  const month = date.getMonth() + 1
+  const day = date.getDate()
+  const hour = date.getHours()
+  const minute = date.getMinutes()
+  const second = date.getSeconds()
+
+  return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
+}
+
+//获取当前日期方法
+const formatDate = date => {
+  const year = date.getFullYear()
+  const month = date.getMonth() + 1
+  const day = date.getDate()
+  return [year, month, day].map(formatNumber).join('-')
+}
+
+//时间格式化  yyyy/MM/dd HH:mm
+const toformatTime = date => {
+  const year = date.getFullYear()
+  const month = date.getMonth() + 1
+  const day = date.getDate()
+  const hour = date.getHours()
+  const minute = date.getMinutes()
+
+
+  return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute].map(formatNumber).join(':')
+}
+
+
+module.exports = {
+  formatTime:formatTime,
+  formatDate: formatDate
+}
+
+module.exports.toformatTime = toformatTime;
+
+
+/**
+ * 数字校验
+ */
+const isNumber = num => {
+  if (!isNaN(num) && num != null && (num + '').length > 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+module.exports.isNumber = isNumber;
+
+//+===========================================================================================================================
+const finalUrl = 'http://localhost:8387/wizardlisa';
+//+===========================================================================================================================
+/**
+ * 请求封装GET
+ */
+var httpGet = function (url, data) {
+  const Authorization = wx.getStorageSync('api_key');
+  const sys_source = wx.getStorageSync('sys_source');
+  var promise = new Promise(function (resolve, reject) {
+    wx.request({
+      url: finalUrl + url,
+      data: data,
+      method: 'GET',
+      header: {
+        'content-type': 'application/json',
+        'Authorization': Authorization,
+        'sys_source': sys_source
+      },
+      success: resolve,
+      fail: reject
+    })
+  });
+  return promise;
+};
+
+module.exports.httpGet = httpGet;
+
+/**
+ * 请求封装POST
+ */
+var httpPost = function (url, data) {
+  const Authorization = wx.getStorageSync('api_key');
+  const sys_source = wx.getStorageSync('sys_source');
+  var promise = new Promise(function (resolve, reject) {
+    wx.request({
+      url: finalUrl + url,
+      data: data,
+      method: 'POST',
+      header: {
+        'content-type': 'application/json',
+        'Authorization': Authorization,
+        'sys_source': sys_source
+      },
+      success: resolve,
+      fail: reject
+    })
+  });
+  return promise;
+};
+
+module.exports.httpPost = httpPost;
+
+/**
+ * POST 上传图片
+ */
+var uploadFile = function (url, filePath, data) {
+  const Authorization = wx.getStorageSync('api_key');
+  const sys_source = wx.getStorageSync('sys_source');
+  var promise = new Promise(function (resolve, reject) {
+    wx.uploadFile({
+      url: finalUrl + url,
+      filePath: filePath,
+      name: 'file',
+      formData: data,
+      header: {
+        'content-type': 'application/json',
+        'Authorization': Authorization,
+        'sys_source': sys_source
+      },
+      success: resolve,
+      fail: reject
+    })
+  });
+  return promise;
+};
+
+module.exports.uploadFile = uploadFile;
+
+
+
+
+/**
+ * 记录小程序用户formId
+ */
+var insertFormId = function (formId) {
+  if (formId && formId.indexOf('form') == -1) {
+    const Authorization = wx.getStorageSync('api_key');
+    const sys_source = wx.getStorageSync('sys_source');
+    wx.request({
+      url: finalUrl + '/notification/formidSave',
+      data: { formId: formId },
+      method: 'POST',
+      header: {
+        'content-type': 'application/json',
+        'Authorization': Authorization,
+        'sys_source': sys_source
+      },
+    })
+  }
+};
+module.exports.insertFormId = insertFormId;
+
+/**
+ * 时间会话列表微信方式显示
+ */
+var geWXtDate = function (dateTimeStamp) {
+  var result = '';
+  var minute = 1000 * 60;
+  var hour = minute * 60;
+  var day = hour * 24;
+  var halfamonth = day * 15;
+  var month = day * 30;
+  var now = Date.parse(new Date());
+  var diffValue = now - dateTimeStamp;
+  var monthC = diffValue / month;
+  var weekC = diffValue / (7 * day);
+  var dayC = diffValue / day;
+  var hourC = diffValue / hour;
+  var minC = diffValue / minute;
+  if (monthC >= 1) {
+    result = "" + parseInt(monthC) + "月前";
+  }
+  else if (weekC >= 1) {
+    result = "" + parseInt(weekC) + "周前";
+  }
+  else if (dayC >= 1) {
+    result = "" + parseInt(dayC) + "天前";
+  }
+  else if (hourC >= 1) {
+    result = "" + parseInt(hourC) + "小时前";
+  }
+  else if (minC >= 1) {
+    result = "" + parseInt(minC) + "分钟前";
+  } else {
+    result = "刚刚";
+  }
+  return result;
+};
+module.exports.geWXtDate = geWXtDate;
+
+//会话 详情 时间
+var geWXtDetailDate = function (dateTimeStamp) {
+  var result = '';
+  const time = new Date(new Date().setHours(0, 0, 0, 0)).getTime();//当天0点时间
+  var date = new Date(dateTimeStamp)//传参数时间
+  const year = date.getFullYear()
+  const month = date.getMonth() + 1
+  const day = date.getDate()
+  const hour = date.getHours()
+  const minute = date.getMinutes()
+  if (dateTimeStamp > time) {//今天内
+    result = [hour, minute].map(formatNumber).join(':')
+  } else {
+    result = [month, day].map(formatNumber).join('.') + ' ' + [hour, minute].map(formatNumber).join(':')
+  }
+  return result;
+};
+module.exports.geWXtDetailDate = geWXtDetailDate;
